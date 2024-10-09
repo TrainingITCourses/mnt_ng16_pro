a59c083925aa7013cc9d8ad4051b9c29dc862277
Alberto Basalo <albertobasalo@hotmail.com>
2024-10-09 17:31:49 +0200

    feat: pre practice

--- AstroBookings/src/app/shared/models/launch.dto.ts
+++ AstroBookings/src/app/shared/models/launch.dto.ts
@@ -10,3 +10,26 @@ export interface LaunchDto {
   pricePerSeat: number;
   status: LaunchStatus;
 }
+
+export const seedData: LaunchDto[] = [
+  {
+    id: '1',
+    agencyId: '1',
+    rocketId: '1',
+    date: new Date(2029, 5, 1),
+    mission: 'Moon Landing',
+    destination: 'Moon',
+    pricePerSeat: 100,
+    status: 'scheduled',
+  },
+  {
+    id: '2',
+    agencyId: '2',
+    rocketId: '2',
+    date: new Date(2033, 8, 2),
+    mission: 'Mars Flight',
+    destination: 'Mars',
+    pricePerSeat: 200,
+    status: 'scheduled',
+  },
+];

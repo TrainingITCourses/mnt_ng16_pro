11c0c6615dee5d5d7cf373057a6f9775d3276bb0
Alberto Basalo <albertobasalo@hotmail.com>
2024-11-06 10:46:48 +0100

    feat: control block

--- AstroBookings/src/app/shared/ui/control.block.ts
+++ AstroBookings/src/app/shared/ui/control.block.ts
@@ -2,84 +2,190 @@ import { Component, forwardRef, Input } from '@angular/core';
 import {
   AbstractControl,
   ControlValueAccessor,
-  FormControl,
+  FormGroup,
   NG_VALUE_ACCESSOR,
 } from '@angular/forms';
 
+/**
+ * A component that renders a form control with a label and an input element and an error message if the control is invalid
+ * - Needs a form, and formControlName as inputs
+ * - Accepts label, placeholder, and type as inputs
+ * - It also accepts arrayName, arrayIndex, and groupName inputs for nested arrays and groups
+ */
 @Component({
   selector: 'app-control',
   providers: [
-    { provide: NG_VALUE_ACCESSOR, useExisting: forwardRef(() => ControlBlock), multi: true },
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => ControlBlock),
+      multi: true,
+    },
   ],
   template: `
     <div>
-      <label [for]="formControlName">{{ label }}:</label>
+      <label [for]="formControlName">
+        {{ label || (formControlName | titlecase) }} <span *ngIf="hasError()">🚨</span>
+      </label>
       <input
         [type]="type"
         [id]="formControlName"
-        [attr.aria-invalid]="invalid"
+        [name]="formControlName"
+        [placeholder]="placeholder || label || formControlName"
+        autocomplete="off"
         [value]="value"
         (input)="onInput($event)"
+        [attr.aria-invalid]="getInvalid()"
       />
-      <small *ngIf="mustDisplayError()" class="error">{{ getDisplayError() }}</small>
+      <small *ngIf="getInvalid()">{{ getErrorMessage() }}</small>
     </div>
   `,
 })
 export class ControlBlock implements ControlValueAccessor {
+  // Inputs
+  /**
+   * The form to which the control belongs
+   * - This is mandatory!
+   */
+  @Input() form!: FormGroup;
+  /**
+   * The name of the control
+   * - This is mandatory!
+   */
+  @Input() formControlName!: string;
+  /**
+   * The text for the label
+   * - Defaults to the formControlName
+   */
   @Input() label = '';
+  /**
+   * The placeholder used as a hint
+   * - Defaults to the label or formControlName
+   */
+  @Input() placeholder = '';
+  /**
+   * The type of the control
+   * - Defaults to 'text'
+   */
   @Input() type = 'text';
-  @Input() formControlName: string = '';
-  value: any;
-  private changeCb: any;
-  touchCb: any;
-  get invalid(): boolean {
+
+  // Group and array inputs
+  /**
+   * The name of the array to search for the control
+   * - Optional, only for sub-arrays
+   */
+  @Input() arrayName?: string;
+  /**
+   * The index of the control in the array
+   * - Optional, only for sub-arrays
+   */
+  @Input() arrayIndex?: number;
+  /**
+   * The name of the group to search for the control
+   * - Optional, only for nested groups
+   */
+  @Input() groupName?: string;
+
+  /**
+   * The current value of the control
+   */
+  value: unknown;
+
+  /**
+   * The function to emit the change event
+   */
+  private changeCallBack: any;
+  /**
+   * The function to emit the touch event
+   */
+  private touchCallBack: any;
+
+  constructor() {}
+
+  // ControlValueAccessor methods
+
+  /**
+   * Stores the value of the control
+   * - This method is called by the ReactiveFormsModule when the value changes programmatically
+   * @param newValue - The value to store
+   */
+  writeValue(newValue: any): void {
+    this.value = newValue;
+  }
+
+  /**
+   * Stores a function to be called when the control value changes
+   * - This method is called once by the ReactiveFormsModule when the component is initialized
+   * @param changeCallBack - The function to emit the change event
+   */
+  registerOnChange(changeCallBack: any): void {
+    this.changeCallBack = changeCallBack;
+  }
+
+  /**
+   * Stores a function to be called when the control is touched
+   * - This method is called once by the ReactiveFormsModule when the component is initialized
+   * @param touchCallBack - The function to emit the touch event
+   */
+  registerOnTouched(touchCallBack: any): void {
+    this.touchCallBack = touchCallBack;
+  }
+
+  // Event handlers
+
+  /**
+   * The event handler for the input event
+   * - notifies the ReactiveFormsModule that the value has changed
+   * @param event - The InputEvent to handle
+   */
+  onInput(event: any) {
+    const target: HTMLInputElement = event.target as HTMLInputElement;
+    this.value = target.value;
+    this.changeCallBack(this.value);
+    this.touchCallBack();
+  }
+
+  // Utility methods
+
+  /**
+   * Checks if the control has an error
+   * @returns true if the control has an error, false otherwise
+   */
+  hasError() {
     return this.getControl().invalid;
   }
   /**
-   * Framework calls this method when the value of the control changes
-   * @param value the new value
+   * Checks if the control should be treated as invalid
+   * @returns undefined if the control is pristine, true if the control is invalid, false otherwise
    */
-  writeValue(value: any): void {
-    this.value = value;
+  getInvalid(): boolean | undefined {
+    if (this.getControl().pristine) {
+      // user has not interacted with the control yet
+      return undefined;
+    }
+    return this.getControl().invalid;
   }
   /**
-   * The framework calls this method on start with a callback function that should be called when the value of the control changes
-   * @param fn the callback function to register on UI change
+   * Gets the error message of the control
+   * @returns the error message of the control
    */
-  registerOnChange(changeCb: any): void {
-    this.changeCb = changeCb;
+  getErrorMessage() {
+    return JSON.stringify(this.getControl().errors || {});
   }
-  registerOnTouched(fn: any): void {
-    this.touchCb = fn;
-  }
-  setDisabledState?(isDisabled: boolean): void {
-    // TODO
-  }
-
-  onInput(event: Event): void {
-    const newValue = event.target ? (event.target as HTMLInputElement).value : null;
-    this.value = newValue;
-    this.changeCb(newValue);
-  }
-
-  mustDisplayError(): boolean {
-    const control = this.getControl();
-    return control.invalid && control.touched;
-  }
-
-  getDisplayError(): string {
-    const control = this.getControl();
-    if (control.hasError('required')) {
-      return 'This field is required';
-    } else if (control.hasError('email')) {
-      return 'Invalid email';
-    } else if (control.hasError('minlength')) {
-      return 'Password must be at least 4 characters';
-    } else {
-      return JSON.stringify(control.errors);
+  /**
+   * Gets the control based on the path at the form
+   * - uses the arrayName, arrayIndex, groupName and formControlName to build the path
+   * @returns the control
+   */
+  private getControl() {
+    const controlPath = [];
+    if (this.arrayName !== undefined) controlPath.push(this.arrayName);
+    if (this.arrayIndex !== undefined) controlPath.push(this.arrayIndex);
+    if (this.groupName !== undefined) controlPath.push(this.groupName);
+    controlPath.push(this.formControlName);
+    let control: AbstractControl | null = this.form.get(controlPath);
+    if (!control) {
+      throw new Error(`Control ${controlPath} not found`);
     }
-  }
-  getControl(): AbstractControl {
-    return new FormControl('');
+    return control;
   }
 }

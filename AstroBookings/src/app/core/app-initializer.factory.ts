95caaa08d633758a9d9b9848e52b1c0445eef7bb
Alberto Basalo <albertobasalo@hotmail.com>
2024-11-13 17:10:35 +0100

    docs: rxjs

--- AstroBookings/src/app/core/app-initializer.factory.ts
+++ AstroBookings/src/app/core/app-initializer.factory.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { LocalStorageService } from '@app/services/local-storage.service';
 import { environment } from 'environments/environment';
-import { catchError, Observable, of, tap } from 'rxjs';
+import { Observable, of } from 'rxjs';
 
 /**
  * App Initializer Factory, creates a function that will be used to initialize the application
@@ -19,12 +19,13 @@ export function appInitializerFactory(
 
 function storeTime(http: HttpClient, localStorage: LocalStorageService): Observable<unknown> {
   const timeZone = environment.timeZone;
-  const url = `https://worldtimeapi.org/api/timezone/${timeZone}`;
-  return http.get(url).pipe(
-    tap((data) => localStorage.write('time', data)),
-    catchError((error) => {
-      console.error(error);
-      return of(null);
-    }),
-  );
+  return of({});
+  // const url = `https://worldtimeapi.org/api/timezone/${timeZone}`;
+  // return http.get(url).pipe(
+  //   tap((data) => localStorage.write('time', data)),
+  //   catchError((error) => {
+  //     console.error(error);
+  //     return of(null);
+  //   }),
+  // );
 }

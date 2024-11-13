9301f36b1e6b3887416448caccd77eca7554d043
Alberto Basalo <albertobasalo@hotmail.com>
2024-11-13 17:58:07 +0100

    feat: concatMap

--- AstroBookings/src/app/routes/launches/launch.page.ts
+++ AstroBookings/src/app/routes/launches/launch.page.ts
@@ -1,7 +1,8 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { AgencyDto } from '@app/models/agency.dto';
 import { LaunchDto } from '@app/models/launch.dto';
-import { Observable } from 'rxjs';
+import { concatMap, Observable } from 'rxjs';
 import { LaunchService } from './launch.service';
 
 @Component({
@@ -9,11 +10,12 @@ import { LaunchService } from './launch.service';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LaunchPage {
-  launchId: string;
-  source$!: Observable<LaunchDto>;
+  launchId: string = this.route.snapshot.paramMap.get('id') || '';
+  launch$: Observable<LaunchDto> = this.launchService.getLaunchById$(this.launchId);
 
-  constructor(private route: ActivatedRoute, private launchService: LaunchService) {
-    this.launchId = this.route.snapshot.paramMap.get('id') || '';
-    this.source$ = this.launchService.getLaunchById$(this.launchId);
-  }
+  agency$: Observable<AgencyDto> = this.launch$.pipe(
+    concatMap((launch: LaunchDto) => this.launchService.getAgencyById$(launch.agencyId)),
+  );
+
+  constructor(private route: ActivatedRoute, private launchService: LaunchService) {}
 }
--- AstroBookings/src/app/routes/launches/launch.service.ts
+++ AstroBookings/src/app/routes/launches/launch.service.ts
@@ -1,16 +1,25 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
+import { AgencyDto } from '@app/models/agency.dto';
 import { LaunchDto } from '@app/models/launch.dto';
+import { AgenciesRepository } from '@app/services/agencies.repository';
 import { LaunchesAbstractRepository } from '@app/services/launches.repository';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LaunchService {
-  constructor(private launchesRepository: LaunchesAbstractRepository) {}
+  constructor(
+    private launchesRepository: LaunchesAbstractRepository,
+    private agenciesRepository: AgenciesRepository,
+  ) {}
 
   getLaunchById$(id: string): Observable<LaunchDto> {
     return this.launchesRepository.getLaunchById$(id);
+  }
+
+  getAgencyById$(agencyId: string): Observable<AgencyDto> {
+    return this.agenciesRepository.getById$(agencyId);
   }
 }
